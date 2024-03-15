3abcb2fee9b5e692caa483a6a2ebfe1524bae716
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-15 17:42:32 +0545

    ff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    'fontFamily': {
+      'open-sans': ['Open Sans', 'sans-serif'],
+      'dosis': ['Dosis', 'sans-serif'],
+      'pacifico': ['Pacifico', 'sans-serif']
+    },
+    'colors': {
+      'primary': {
+        500: '#337ab7'
+      }
+    }
+  },
+  plugins: [
+    require('flowbite/plugin')
+    ]
 }
+
