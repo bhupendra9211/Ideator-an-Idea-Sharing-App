fe6dde69f65f9d653bd5a2e5284f68eb5b339962
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-18 12:08:23 +0545

    adding new files

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,6 +5,30 @@ class IdeasController < ApplicationController
   
     def create
       @idea = Idea.create(idea_params)
+      if @idea.valid?
+        # Implement later
+      else
+        # Implement later
+      end
+      redirect_to root_path
+    end
+
+    def edit
+      @idea = Idea.find(params[:id])
+    end
+
+    def update
+      @idea = Idea.find(params[:id])
+      if @idea.update(idea_params)
+        redirect_to root_path
+      else
+        redirect_to edit_idea_path(params[:id])
+      end
+    end
+    
+    def destroy
+      @idea = Idea.find(params[:id])
+      @idea.destroy
       redirect_to root_path
     end
   
